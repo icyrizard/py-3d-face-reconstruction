bafb7dbd41bad2fb69f3c7ff26fa6b5375e015d8
Richard Torenvliet <richard@torenvliet.org>
2016-04-17 19:31:48 +0200

    Add a way to store the pca model and add the pca model itself

--- src/main.py
+++ src/main.py
@@ -10,7 +10,6 @@ from imm_points import IMMPoints, build_feature_vectors, \
     flatten_feature_vectors
 
 
-
 def nothing(_):
     pass
 
@@ -19,6 +18,11 @@ def add_parser_options():
     parser = argparse.ArgumentParser(description='IMMPoints tool')
 
     pca_group = parser.add_argument_group('show_pca')
+
+    pca_group.add_argument(
+        '--store_pca', action='store_true',
+        help='number of principle components to keep and are able to manipulate'
+    )
 
     pca_group.add_argument(
         '--show_pca', action='store_true',
@@ -35,6 +39,11 @@ def add_parser_options():
         help='number of principle components to keep and are able to manipulate'
     )
 
+    pca_group.add_argument(
+        '--file', type=str,
+        help='pca model file that contains or is going to contain the pca model'
+    )
+
     return parser
 
 
@@ -45,13 +54,34 @@ def init_eigenvalue_trackbars(n_components, s):
         cv2.createTrackbar('{}'.format(i), 'eigenvalues', 500, 1000, nothing)
 
 
-def show_pca():
-    assert args.asf, '--asf files should be supplied'
+def store_model(args):
+    """
+    Store the U, s, Vt and mean of all the asf datafiles given by the asf
+    files.
+    """
+    assert args.asf, '--asf files should be given'
+    assert args.file, '--file needs to be provided to store the pca model'
 
     imm_points = build_feature_vectors(args.asf, flattened=True)
     mean_values = get_mean(imm_points)
 
     U, s, Vt = pca(imm_points, mean_values)
+
+    np.save(args.file, np.asarray([U, s, Vt, mean_values]))
+
+
+def show_pca(args):
+    assert args.asf, '--asf files should be given to allow the image to be shown'
+    assert args.file, '--file needs to be provided to get the pca model'
+
+    # load the stored model file
+    UsVtm = np.load(args.file)
+
+    U = UsVtm[0]
+    s = UsVtm[1]
+    Vt = UsVtm[2]
+
+    mean_values = UsVtm[3]
 
     # init trackbars
     index = 0
@@ -73,7 +103,8 @@ def show_pca():
         imm.show_on_img(img)
 
         for i in range(n_components):
-            s_copy[i] = s[i] * ((cv2.getTrackbarPos(str(i), 'eigenvalues') - 500) / 10.0)
+            s_copy[i] = s[i] * (
+                    (cv2.getTrackbarPos(str(i), 'eigenvalues') - 500) / 10.0)
 
         index = cv2.getTrackbarPos('index', 'index')
         imm = IMMPoints(filename=args.asf[index])
@@ -86,9 +117,15 @@ def show_pca():
     cv2.destroyAllWindows()
 
 
-if __name__ == '__main__':
+def main():
     parser = add_parser_options()
     args = parser.parse_args()
 
     if args.show_pca:
-        show_pca()
+        show_pca(args)
+    elif args.store_pca:
+        store_model(args)
+
+
+if __name__ == '__main__':
+    main()
